3164cbfd50e81ea8a9ed4aac894bc83a5386d95e
KnGr00 <57843246+KnGr00@users.noreply.github.com>
2023-03-15 13:42:53 +0300

    Addition to minimize command prompt

--- Clipboard_to_Image.py
+++ Clipboard_to_Image.py
@@ -2,18 +2,21 @@ from PIL import ImageGrab
 from os import getcwd,getlogin,environ,system
 from datetime import datetime
 from winsound import PlaySound,SND_FILENAME
+from pyautogui import hotkey
 
-now = datetime.now() # To get time
+hotkey("win","down")  # to minimize command prompt
+
+now = datetime.now()  # To get time
 date_now = str(now.day)+"."+str(now.month)+"."+str(now.year)+" "+str(now.hour)+"."+str(now.minute)+"."+str(now.second) # To have time as variable as desired format
 
-img = ImageGrab.grabclipboard() # Get picture from clipboard as variable
+img = ImageGrab.grabclipboard()  # Get picture from clipboard as variable
 
-if (img is None) or (type(img) is list): # Control of if item is not picture and handling stiuations (item == text or empty)
-    PlaySound("Windows Foreground.wav",SND_FILENAME) #playing error sound
+if (img is None) or (type(img) is list):  # Control of if item is not picture and handling stiuations (item == text or empty)
+    PlaySound("Windows Foreground.wav",SND_FILENAME)  # playing error sound
     exit()
-else: #Easy code reading purposes
+else:  # Easy code reading purposes
     pass
 
 
-img.save(environ["HOMEPATH"] + "\Desktop\\" + date_now + ".png") #saving image to desired location. Using environ makes sure to get users homepath
+img.save(environ["HOMEPATH"] + "\Desktop\\" + date_now + ".png")  # saving image to desired location. Using environ makes sure to get users homepath
 # currently it's set to save image to desktop location.
